9654fb39aedf7e059f0673d0eec82fc08223bea6
sreevaishnavirao <vaishnavisree213@gmail.com>
2024-07-02 18:01:51 -0400

    header components created

--- src/App.jsx
+++ src/App.jsx
@@ -4,10 +4,18 @@ import Signin from './pages/Signin';
 import Signup from './pages/Signup';
 import About from './pages/About';
 import Profile from './pages/Profile';
+import Header from './components/Header.jsx'
+
+
+
 
 export default function App() {
   return ( 
   <BrowserRouter>
+  <Header/>
+ 
+
+
   <Routes>
     <Route path='/' element={<Home />} />
     <Route path='/sign-in' element={<Signin />} />
